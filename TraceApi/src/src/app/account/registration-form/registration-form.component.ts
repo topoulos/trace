923bf17693fb46de065dfbe86181d4b9c4735f5a
Tony Angelopolopolopo <zoetechllc@gmail.com>
2018-07-18 02:01:57 -0500

    cleanup and refactor a bit.  Add name to text.

--- TraceApi/src/src/app/account/registration-form/registration-form.component.ts
+++ TraceApi/src/src/app/account/registration-form/registration-form.component.ts
@@ -5,37 +5,39 @@ import { UserRegistration } from '../../shared/models/user.registration.interfac
 import { UserService } from '../../shared/services/user.service';
 
 @Component({
-  selector: 'app-registration-form',
-  templateUrl: './registration-form.component.html',
-  styleUrls: ['./registration-form.component.scss']
+    selector: 'app-registration-form',
+    templateUrl: './registration-form.component.html',
+    styleUrls: ['./registration-form.component.scss']
 })
 export class RegistrationFormComponent implements OnInit {
 
-  errors: string;  
-  isRequesting: boolean;
-  submitted: boolean = false;
+    errors: string;
+    isRequesting: boolean;
+    submitted = false;
 
-  constructor(private userService: UserService,private router: Router) { }
+    constructor(private userService: UserService, private router: Router) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  registerUser({ value, valid }: { value: UserRegistration, valid: boolean }) {
-    this.submitted = true;
-    this.isRequesting = true;
-    this.errors='';
-    if(valid)
-    {
-        this.userService.register(value.email,value.password,value.firstName,value.lastName,value.location)
-                  .finally(() => this.isRequesting = false)
-                  .subscribe(
-                    result  => {if(result){
-                        this.router.navigate(['/login'],{queryParams: {brandNew: true,email:value.email}});                         
-                    }},
-                    errors =>  this.errors = errors);
-    }      
- } 
+    registerUser({ value, valid }: { value: UserRegistration, valid: boolean }) {
+        this.submitted = true;
+        this.isRequesting = true;
+        this.errors = '';
 
-   
+        if (valid) {
+            this.userService.register(value.email, value.password, value.firstName, value.lastName, value.location)
+                .finally(() => this.isRequesting = false)
+                .subscribe(
+                    result => {
+                        if (result) {
+                            this.router.navigate(['/login'], { queryParams: { brandNew: true, email: value.email } });
+                        }
+                    },
+                    errors => this.errors = errors);
+        }
+    }
+
+
 
 }
--- TraceApi/src/src/app/header/header.component.ts
+++ TraceApi/src/src/app/header/header.component.ts
@@ -1,32 +1,32 @@
-import { Component, OnInit,OnDestroy } from '@angular/core';
-import {Subscription} from 'rxjs/Subscription';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 
 import { UserService } from '../shared/services/user.service';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
 
-export class HeaderComponent implements OnInit,OnDestroy {
+export class HeaderComponent implements OnInit, OnDestroy {
 
-  status: boolean;
- subscription:Subscription;
+    status: boolean;
+    subscription: Subscription;
 
-  constructor(private userService:UserService) {     
-   }
+    constructor(private userService: UserService) {
+    }
 
-   logout() {
-     this.userService.logout();       
-  }
+    logout() {
+        this.userService.logout();
+    }
 
-  ngOnInit() {
-    this.subscription = this.userService.authNavStatus$.subscribe(status => this.status = status);
-  }
+    ngOnInit() {
+        this.subscription = this.userService.authNavStatus$.subscribe(status => this.status = status);
+    }
 
-   ngOnDestroy() {
-    // prevent memory leak when component is destroyed
-    this.subscription.unsubscribe();
-  }
+    ngOnDestroy() {
+        // prevent memory leak when component is destroyed
+        this.subscription.unsubscribe();
+    }
 }
